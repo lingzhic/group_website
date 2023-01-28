81b79805bc4f1a2ae854dba9914df53b82c43e6d
lingzhic <lingzhic@student.unimelb.edu.au>
2023-01-28 12:14:01 +1100

    update

--- webproject/settings/production.py
+++ webproject/settings/production.py
@@ -4,5 +4,5 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['52.62.10.15', '127.0.0.1', 'localhost ', '*']
+ALLOWED_HOSTS = ['3.104.123.76', '127.0.0.1', 'localhost ', '*']
 # HAYSTACK_CONNECTIONS['default']['URL'] = 'http://hellodjango_blog_tutorial_elasticsearch:9200/'
