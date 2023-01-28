ea133628b3450740ef50342676bf3a9728710000
lingzhic <lingzhic@student.unimelb.edu.au>
2023-01-28 11:09:35 +1100

    update

--- webproject/settings/local.py
+++ webproject/settings/local.py
@@ -4,7 +4,7 @@ SECRET_KEY = 'development-secret-key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '*']
 
 # 搜索设置
 # HAYSTACK_CONNECTIONS['default']['URL'] = 'http://elasticsearch_local:9200/'
